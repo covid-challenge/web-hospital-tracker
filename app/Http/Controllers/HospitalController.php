1e56a8c62a6b9d669df82d8c778e3c2e183868f9
Aaron H <probablynotbutmostdefinitely@gmail.com>
2020-05-30 17:57:41 +0800

    Fixed where clicking the search results makes the map freeze in place.

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -18,21 +18,15 @@ class HospitalController extends Controller
         $hospitals = Hospital::where('name', '<>', '')->where('lat', '<>', '')->where('lng', '<>', '')->get();
         return view('index', compact('hospitals'));
     }
+    
     public function searchHospital(Request $search){
-
-      DB::beginTransaction();
-
       try {
         $data = $search->data;
 
         $hospitals = Hospital::where('name', 'like', '%' . $data . '%')
                              ->orWhere('city', 'like', '%' . $data . '%')->take(30)->get();
-
-        DB::commit();
         return new ResponseResource($hospitals);
-
       } catch (\Exception $e) {
-        DB::rollback();
         return new ResponseResource($e);
       }
     }
