3e4701b82496bed27acc6f6530d88a316d435f57
Aaron H <probablynotbutmostdefinitely@gmail.com>
2020-05-31 00:29:18 +0800

    Moved chloropleth map function to custom.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -37,58 +37,6 @@
                 return $word.toUpperCase();
             });
         }
-
-        function addChloroplethMap()
-        {
-            var geojsonLayer = $.getJSON("philippines_pop_density.geojson", function(data){
-                L.geoJson(data, {
-                    style: style,
-                    onEachFeature: onEachFeature
-                }).addTo(map);
-            });
-
-            // get color depending on population density value
-            function getColor(density) {
-                if (density > 1000)
-                    return "#800026";
-                else if (density > 500)
-                    return "#BD0026";
-                else if (density > 200)
-                    return "#E31A1C";
-                else if (density > 100)
-                    return "#FC4E2A";
-                else if (density > 50)
-                    return "#FD8D3C";
-                else if (density > 20)
-                    return "#FEB24C";
-                else if (density > 10)
-                    return "#FED976";
-                else
-                    return "#FFEDA0";
-            }
-            
-            function style(feature) {
-                return {
-                    weight: 2,
-                    opacity: 0,
-                    color: "black",
-                    dashArray: "3",
-                    fillOpacity: 0.7,
-                    fillColor: getColor(feature.properties.DN)
-                };
-            }
-
-            function zoomToFeature(e) {
-                map.fitBounds(e.target.getBounds());
-            }
-
-            function onEachFeature(feature, layer) {
-                layer.on({
-                    click: zoomToFeature
-                });
-            }
-
-        }
     </script>
 
 @endpush
